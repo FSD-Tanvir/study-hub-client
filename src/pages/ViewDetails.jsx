3c43b1134eda0b9db6d1842eb6710908dfc1e6e1
tanvirfsd <tahasanpro@gmail.com>
2023-11-07 14:17:53 +0600

    create delete function to delete an assignment

--- src/pages/ViewDetails.jsx
+++ src/pages/ViewDetails.jsx
@@ -1,24 +1,48 @@
-import { Link, useLoaderData } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link, useLoaderData, useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const ViewDetails = () => {
   const assignment = useLoaderData();
-  const { title, image, description, dueDate, difficulty,marks } = assignment || {};
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const userEmail = user.email;
+  const { _id, title, image, description, dueDate, difficulty, marks, email } =
+    assignment || {};
+
+  const handleDelete = (_id) => {
+    axios
+      .delete(`http://localhost:5000/api/v1/all-assignments/${_id}`, {
+        data: { userEmail }, // Use a data property to pass the email
+      })
+      .then((response) => {
+        if (response.data.deletedCount > 0) {
+          toast.success(" Deleted Successfully");
+          navigate("/all-assignments");
+        }
+      })
+      .catch((error) => {
+        toast.error("Error:", error);
+      });
+  };
+
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero block mt-16">
       <div className="hero-content flex-col md:flex-row-reverse">
         <img
           src={image}
           className="max-w-sm w-64 sm:w-full mx-auto  rounded-lg  md:h-96 object-cover"
         />
         <div>
-          <div >
+          <div>
             <h1 className="text-3xl font-semibold  text-blue-600 ">{title}</h1>
             <p className="py-6">{description}</p>
           </div>
 
           <div className="flex justify-between my-5">
-            <span>Difficulty Level:  {difficulty}</span>
-            <span>Marks:  {marks}</span>
+            <span>Difficulty Level: {difficulty}</span>
+            <span>Marks: {marks}</span>
           </div>
 
           <div className="flex items-center justify-between">
@@ -35,6 +59,37 @@ const ViewDetails = () => {
             </span>
           </div>
         </div>
+      </div>
+
+      {/* if i want show delete button conditionally */}
+
+      {/* <div>
+        {user.email === email ? (
+          <div className="text-center my-10">
+            <Link>
+              <button
+                className=" px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-red-600 hover:bg-red-700  rounded-xl "
+                type="submit"
+              >
+                Delete This Assignment
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div> */}
+
+      <div className="text-center my-10">
+        <Link>
+          <button
+            className=" px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-red-600 hover:bg-red-700  rounded-xl "
+            type="submit"
+            onClick={() => handleDelete(_id)}
+          >
+            Delete This Assignment
+          </button>
+        </Link>
       </div>
     </div>
   );
